2751826c864b4be04df5d5f4608c3860c2ab735b
sabulous <sabsah@gmail.com>
2021-08-26 11:50:16 +0300

    Move the codebase into src folder

--- src/services/pokemon.service.js
+++ src/services/pokemon.service.js
@@ -2,24 +2,29 @@ import axios from "axios";
 import { shakespeareService } from "../index.js";
 
 const POKEMON_API = process.env.POKEMON_API;
+
 class PokemonService {
   constructor() {}
 
   async getDescriptionByName(name) {
-    const res = await axios.get(`${POKEMON_API}/${name}/`);
-    const descriptions = res?.data?.flavor_text_entries;
-    let englishDescriptions = [];
+    try {
+      const res = await axios.get(`${POKEMON_API}/${name}/`);
+      const descriptions = res?.data?.flavor_text_entries;
+      let englishDescriptions = [];
 
-    if (descriptions) {
-      for (const description of descriptions) {
-        if (description.language.name === "en") {
-          englishDescriptions.push(description.flavor_text);
+      if (descriptions) {
+        for (const description of descriptions) {
+          if (description.language.name === "en") {
+            englishDescriptions.push(description.flavor_text);
+          }
         }
+        const formatted = englishDescriptions[0].replace(/[\n\f\t\r]/g, " "); // replace escape characters
+        return formatted;
+      } else {
+        return null;
       }
-      const formatted = englishDescriptions[0].replace(/[\n\f\t\r]/g, " "); // replace escape characters
-      return formatted;
-    } else {
-      return null;
+    } catch (e) {
+      throw new Error("Error: Description is null.");
     }
   }
 
---
+++ src/services/shakespeare.service.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const SHAKESPEARE_API = process.env.SHAKESPEARE_API;
+
+class ShakespeareService {
+  constructor() {}
+
+  async translate(text) {
+    try {
+      const payload = { text };
+      const res = await axios.post(`${SHAKESPEARE_API}`, payload);
+      return res?.data?.contents?.translated;
+    } catch (e) {
+      throw new Error(
+        "Error: A problem occurred while translating description."
+      );
+    }
+  }
+}
+
+export default ShakespeareService;
